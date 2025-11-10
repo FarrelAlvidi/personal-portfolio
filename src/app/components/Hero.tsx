dee3fc5d07611a732ed4c000eadf7c3290195947
Heddi Heryadi <heddiheryadi@gmail.com>
2025-11-10 19:10:47 +0700

    s

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -26,7 +26,7 @@ export const Hero = () => {
             Farrel Alvidi
           </motion.h1>
         </div>
-        {/* <motion.div
+        <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1.5, delay: 0.5 }}
@@ -39,7 +39,7 @@ export const Hero = () => {
               Frontend Developer - Junior Web Developer - &nbsp;
             </ScrollVelocityRow>
           </ScrollVelocityContainer>
-        </motion.div> */}
+        </motion.div>
         
       </div>
     </>
